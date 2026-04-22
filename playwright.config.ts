407117ab70a6d7fc8d61ceeca01c9212ea0a6775
Juan Cisterna <juancisterna@Juans-MacBook-Pro.local>
2026-04-22 15:40:32 -0400

    Primera versión de framework

---
+++ playwright.config.ts
@@ -0,0 +1,42 @@
+import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
+import path from 'node:path';
+
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    trace: 'on',
+    screenshot: 'on',
+  },
+
+  projects: [
+    /*{
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'Edge',
+      use: { ...devices['Desktop Edge'], channel: 'msedge',},
+    },*/
+    {
+      name: 'google chrome',
+      use: { ...devices['Desktop Chrome'], channel: 'chrome',},
+    },
+  ],
+});
